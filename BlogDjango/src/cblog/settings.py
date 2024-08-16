ad5dd4fa3f3b76978c883a0dc808cf7b56e38873
Melis Filiz Esenkaya <127357851+Filiz5@users.noreply.github.com>
2024-08-16 13:43:55 +0200

    Update2.py

--- BlogDjango/src/cblog/settings.py
+++ BlogDjango/src/cblog/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'fprds', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
-        'PASSWORD': config('PASSWORD'),#FinalProject2024!
+        'PASSWORD': config('PASSWORD'),#Admin1234
         'HOST': 'fprds.cdaiqg202bit.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
@@ -144,7 +144,7 @@ LOGIN_REDIRECT_URL = "blog:list"
 LOGIN_URL = "login"
 
 
-AWS_STORAGE_BUCKET_NAME = 'fpbucketmelis' # please enter your s3 bucket name
+AWS_STORAGE_BUCKET_NAME = 'fp-bucket-melis' # please enter your s3 bucket name
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region 
 AWS_DEFAULT_ACL = 'public-read'
